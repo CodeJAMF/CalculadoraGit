ed2fec0d61667de1b079ad7535041e130c7ec8ce
Jose Angel Martinez <117295105+Jamartinez624@users.noreply.github.com>
2023-01-23 11:50:31 +0100

    Nueva funcionalidad multiplica

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -13,4 +13,9 @@ public class Calculadora {
     public double division (double a, double b){
         return a/b;
     }
+
+    public double multiploica (double a, double b){
+        return a*b;
+
+    }
 }
